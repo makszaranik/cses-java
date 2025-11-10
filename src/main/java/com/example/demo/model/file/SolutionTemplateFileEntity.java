b9104e03ae4aaf92c9d524ffc9429188750c646b
makszaranik <zaranikXZ@gmail.com>
2025-11-10 16:42:10 +0200

    jobs now independent from filename:

---
+++ src/main/java/com/example/demo/model/file/SolutionTemplateFileEntity.java
@@ -0,0 +1,4 @@
+package com.example.demo.model.file;
+
+public class SolutionTemplateFileEntity {
+}
---
+++ src/main/java/com/example/demo/service/executor/stage/DockerJobRunner.java
@@ -0,0 +1,4 @@
+package com.example.demo.service.executor.stage;
+
+public class DockerJobRunner {
+}
