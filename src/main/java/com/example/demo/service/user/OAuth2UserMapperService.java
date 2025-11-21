f7bdc74c417b1795d3cd618aded703c1ecee3a5c
makszaranik <zaranikXZ@gmail.com>
2025-11-21 13:03:04 +0200

    remove imports

--- src/main/java/com/example/demo/service/user/OAuth2UserMapperService.java
+++ src/main/java/com/example/demo/service/user/OAuth2UserMapperService.java
@@ -2,23 +2,14 @@ package com.example.demo.service.user;
 
 import com.example.demo.model.user.UserEntity;
 import com.example.demo.repository.UserRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
